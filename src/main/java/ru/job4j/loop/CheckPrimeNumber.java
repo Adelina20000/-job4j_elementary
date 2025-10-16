28caf840481d42796b5c552ef29a188f6409c33b
Natalya <Natalya.B2000@yandex.ru>
2025-10-16 23:14:55 +0500

    Простое число (оператор прерывания break) [#505214]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+
+    public static boolean check(int number) {
+
+        if (number < 2) {
+            return false;
+        }
+
+        for (int index = 2; index <= number - 1; index++) {
+            if (number % index == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CheckPrimeNumberTest {
+
+    @Test
+    public void when5ThenTrue() {
+        int number = 5;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void when4ThenFalse() {
+        int number = 4;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void when1ThenFalse() {
+        int number = 1;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void when11ThenTrue() {
+        int number = 11;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void when25ThenFalse() {
+        int number = 25;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void when9ThenFalse() {
+        int number = 9;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void when49ThenFalse() {
+        int number = 49;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+}
